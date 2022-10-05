2b5bc13cff1d067e546370c31df8b94ec88195ee
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-10-05 15:54:52 +0300

    Linked List class is created and basic methods added.

---
+++ src/DataStructureLab/Week3_LinkedList.java
@@ -0,0 +1,33 @@
+package DataStructureLab;
+
+public class Week3_LinkedList {
+    class Node<E>{
+        //Attributes
+        private E value;
+        private Node<E> next;
+
+        //Constructor
+        public Node(E value){
+            this.value=value;
+        }
+
+        //Getter and Setters
+        public E getValue() {
+            return value;
+        }
+        public void setValue(E value) {
+            this.value = value;
+        }
+
+
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
