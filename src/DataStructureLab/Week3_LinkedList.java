1c5c4d3bf0cf8c50213311a3da3b25ed4dcdb9bb
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-10-05 16:06:10 +0300

    Linked List class is created.

--- src/DataStructureLab/Week3_LinkedList.java
+++ src/DataStructureLab/Week3_LinkedList.java
@@ -1,33 +1,42 @@
 package DataStructureLab;
 
-public class Week3_LinkedList {
-    class Node<E>{
-        //Attributes
-        private E value;
-        private Node<E> next;
+class Node<E>{
+    //Attributes
+    private E value;
+    private Node<E> next;
 
-        //Constructor
-        public Node(E value){
-            this.value=value;
-        }
+    //Constructor
+    public Node(E value){
+        this.value=value;
+    }
 
-        //Getter and Setters
-        public E getValue() {
-            return value;
-        }
-        public void setValue(E value) {
-            this.value = value;
-        }
-
-
+    //Getter and Setters
+    public E getValue() {
+        return value;
+    }
+    public void setValue(E value) {
+        this.value = value;
     }
 
 
 
+}
+
+class LinkedList<E>{
+    private Node<E> head;
+    private Node<E> tail;
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+}
 
 
+
+public class Week3_LinkedList {
 
     public static void main(String[] args) {
+        LinkedList<String> myLinkedList = new LinkedList<>();
 
     }
 }
