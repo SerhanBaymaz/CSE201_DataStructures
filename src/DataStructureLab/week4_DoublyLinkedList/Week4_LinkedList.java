2b3ee94d38f671c83a134258b582b47d1e5c59d4
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-10-12 15:41:41 +0300

    code implemented from last week.

---
+++ src/DataStructureLab/week4_DoublyLinkedList/Week4_LinkedList.java
@@ -0,0 +1,73 @@
+package DataStructureLab.week4_DoublyLinkedList;
+
+public class Week4_LinkedList {
+    public static void main(String[] args) throws Exception {
+        LinkedList<Integer> myList = new LinkedList<Integer>();
+        myList.addLast(5);
+        myList.addLast(15);
+        myList.addLast(27);
+        myList.addLast(91);
+        myList.print();
+
+    }
+}
+
+class LinkedList<E>{
+    Node<E> head;
+    Node<E> tail;
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void addLast(E value){
+        Node<E> newNode = new Node<E>(value);
+        if(isEmpty()){
+            head = newNode;
+        }
+        else{
+            tail.setNext(newNode);
+        }
+        tail = newNode;
+    }
+
+    public void print(){
+        this.print(head);
+    }
+
+    private void print(Node<E> node){
+        if(node == null)
+            return;
+
+        System.out.println(node.getValue());
+        print(node.getNext());
+    }
+
+}
+
+class Node<E>{
+    private E value;
+    private Node<E> next;
+
+    public Node(E value){
+        this.value = value;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+}
+
