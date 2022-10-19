a09fa9f81f793b94582210cef51308d06ce24470
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-10-19 16:38:44 +0300

    StackArray class created. You can use it, it works.

--- src/DataStructureLab/week5_StackArray/StackArray.java
+++ src/DataStructureLab/week5_StackArray/StackArray.java
@@ -1,7 +1,8 @@
 package DataStructureLab.week5_StackArray;
 
-public class StackArray<E> {
+public class StackArray<E> implements IStack<E> {
 
+    /*
     private static class Node<E> {
         //attributes
 
@@ -12,13 +13,19 @@ public class StackArray<E> {
         //Methods
 
     }
-
+    */
 
     //Stack Attributes
-
-
+    //private Node<E> top;
+    private E[] data;
+    private int size;
 
     //Stack Constructor
+    public StackArray() {
+        //top = null;
+        data = (E[]) new Object[1000];
+    }
+
 
 
     //Stack Methods
@@ -32,10 +39,56 @@ public class StackArray<E> {
     3. pop()  : The element is popped from the top of the stack and is removed.
      */
 
+    @Override
+    public void push(E item) {
+        data[size++] = item;
+    }
+
+    @Override
+    public E pop() {
+        return data[--size];
+    }
+
+    @Override
+    public E top() {
+        return data[size - 1];
+    }
+
+    @Override
+    public boolean isEmpty() {
+           return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    //To string
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < size; i++) {
+            s += data[i] + ", ";
+        }
+        return "[" + s + "]";
+    }
+
 
     public static void main(String[] args) {
         System.out.println("************************");
 
+        StackArray<Integer> stack = new StackArray<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        System.out.println("Stack Size: " + stack.size());
+        System.out.println("Stack Top: " + stack.top());
+        System.out.println("Stack Pop: " + stack.pop());
+        System.out.println("Stack Size: " + stack.size());
+        System.out.println("Stack: " + stack);
 
         System.out.println("************************");
     }
