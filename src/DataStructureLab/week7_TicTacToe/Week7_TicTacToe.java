cfee5c7a547b15191f34ea95f9572a76a9fc59e9
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-11-02 15:44:48 +0300

    Board class has created.

---
+++ src/DataStructureLab/week7_TicTacToe/Week7_TicTacToe.java
@@ -0,0 +1,88 @@
+package DataStructureLab.week7_TicTacToe;
+class Board {
+    public static final int X = 1, O = -1, EMPTY = 0;
+    private int[][] boardArray = new int[3][3];
+    private int player;
+
+    Board() {
+        clearBoard();
+    }
+
+    public void clearBoard() {
+        for (int i = 0; i < boardArray.length; i++) {
+            for (int j = 0; j < boardArray[0].length; j++) {
+                boardArray[i][j] = EMPTY;
+            }
+        }
+        player = X;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < boardArray.length; i++) {
+            for (int j = 0; j < boardArray[0].length; j++) {
+                if (boardArray[i][j] == 1)
+                    System.out.print("X  ");
+                else if (boardArray[i][j] == -1)
+                    System.out.print("O  ");
+                else
+                    System.out.print("-  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public boolean isWin(int a) {
+        if (boardArray[0][0] + boardArray[0][1] + boardArray[0][2] == 3 * a) {
+            return true;
+        } else if (boardArray[1][0] + boardArray[1][1] + boardArray[1][2] == 3 * a) {
+            return true;
+        } else if (boardArray[2][0] + boardArray[2][1] + boardArray[2][2] == 3 * a) {
+            return true;
+        } else if (boardArray[0][0] + boardArray[1][0] + boardArray[2][0] == 3 * a) {
+            return true;
+        } else if (boardArray[0][1] + boardArray[1][1] + boardArray[2][1] == 3 * a) {
+            return true;
+        } else if (boardArray[0][2] + boardArray[1][2] + boardArray[2][2] == 3 * a) {
+            return true;
+        } else if (boardArray[0][0] + boardArray[1][1] + boardArray[2][2] == 3 * a) {
+            return true;
+        } else if (boardArray[2][0] + boardArray[1][1] + boardArray[0][2] == 3 * a) {
+            return true;
+        } else
+            return false;
+
+    }
+
+    public void putMark(int i, int j) throws IllegalAccessException {
+        if (i < 0 || i > 2 || j < 0 || j > 2)
+            throw new IllegalArgumentException("Invalid board position!");
+        if (boardArray[i][j] != EMPTY)
+            throw new IllegalArgumentException("Board position occupied!");
+        boardArray[i][j] = player;
+        player = -player;
+    }
+
+    public int winner() {
+        if (isWin(X))
+            return (X);
+        else if (isWin(O))
+            return (O);
+        else
+            return (0);
+    }
+}
+
+public class Week7_TicTacToe {
+    public static void main(String[] args) throws Exception {
+        Board game1 = new Board();
+        game1.printBoard();
+        game1.putMark(1, 1);
+        game1.printBoard();
+        game1.putMark(1, 2);
+        game1.printBoard();
+    }
+}
+
+
+
