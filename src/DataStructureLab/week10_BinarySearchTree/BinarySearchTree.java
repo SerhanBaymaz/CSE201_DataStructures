17242271854440a7b6441a9ac60f8f0332ad9f7a
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-11-30 16:02:57 +0300

    BinarySearchTree class created.

---
+++ src/DataStructureLab/week10_BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package DataStructureLab.week10_BinarySearchTree;
+
+public class BinarySearchTree {
+    //Attributes
+    public Node root;
+
+    //Constructor
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+
+
+
+
+
+}
