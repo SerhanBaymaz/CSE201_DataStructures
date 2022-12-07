b479ea1faa0bcb6078e6b6247349e8d849d07d74
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-12-07 15:31:35 +0300

    insert method added.

--- src/DataStructureLab/week11_AVLtree/AVL_Tree.java
+++ src/DataStructureLab/week11_AVLtree/AVL_Tree.java
@@ -50,7 +50,6 @@ public class AVL_Tree {
     }
 
     public Node leftRotate(Node node) {
-        //write example for left rotate to understand
         /*
 
         node
@@ -90,6 +89,56 @@ public class AVL_Tree {
             return 0;
         }
         return getHeight(node.left) - getHeight(node.right);
+    }
+
+    public Node insert(Node node, int key) {
+        /* 1.  Perform the normal BST insertion */
+        if (node == null) {
+            return (new Node(key));
+        }
+
+        if (key < node.key) {
+            node.left = insert(node.left, key);
+        } else if (key > node.key) {
+            node.right = insert(node.right, key);
+        } else // Duplicate keys not allowed
+        {
+            return node;
+        }
+
+        /* 2. Update height of this ancestor node */
+        node.height = 1 + max(getHeight(node.left),getHeight(node.right));
+
+        /* 3. Get the balance factor of this ancestor
+              node to check whether this node became
+              unbalanced */
+        int balance = getBalance(node);
+
+        // If this node becomes unbalanced, then there
+        // are 4 cases Left Left Case
+        if (balance > 1 && key < node.left.key) {
+            return rightRotate(node);
+        }
+
+        // Right Right Case
+        if (balance < -1 && key > node.right.key) {
+            return leftRotate(node);
+        }
+
+        // Left Right Case
+        if (balance > 1 && key > node.left.key) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        // Right Left Case
+        if (balance < -1 && key < node.right.key) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
+        /* return the (unchanged) node pointer */
+        return node;
     }
 
 
--- src/DataStructureLab/week11_AVLtree/App.java
+++ src/DataStructureLab/week11_AVLtree/App.java
@@ -3,6 +3,12 @@ package DataStructureLab.week11_AVLtree;
 public class App {
     public static void main(String[] args) {
         AVL_Tree tree = new AVL_Tree();
+        tree.root = tree.insert(tree.root, 15);
+        tree.root = tree.insert(tree.root, 20);
+        tree.root = tree.insert(tree.root, 30);
+        tree.root = tree.insert(tree.root, 40);
+        tree.root = tree.insert(tree.root, 50);
+        tree.root = tree.insert(tree.root, 25);
 
     }
 }
